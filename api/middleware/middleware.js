b80821ba01b26ea3463dced11eb7695767e503d5
jaredhall18 <jaredhall18@gmail.com>
2021-12-01 15:04:45 -0700

    logger is working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -2,19 +2,26 @@ const Users = require('./../users/users-model')
 const Posts = require('./../posts/posts-model')
 
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`${timeStamp} ${method} ${url} `)
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  next();
 }
 
 // do not forget to expose these functions to other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -6,12 +6,15 @@ const {
   validatePost
 } = require('./../middleware/middleware')
 const Users = require('./users-model')
-const Posts = require('./../posts/posts-model')
+const Posts = require('./../posts/posts-model');
+const server = require('../server');
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
+
+
 
 router.get('/', (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
